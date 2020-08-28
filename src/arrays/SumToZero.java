6e161efef321beebb24f4f56f07a6a7fea10d7c8
neetika-Sri <neetika.srivastava@gmail.com>
2020-08-28 12:38:21 -0700

    Add SumToZero

---
+++ src/arrays/SumToZero.java
@@ -0,0 +1,73 @@
+package arrays;
+
+import java.util.*;
+
+public class SumToZero {
+
+    public static boolean isThreeSum(int[] inpArr, int val) {
+        if (inpArr == null || inpArr.length < 3)
+            return false;
+        for (int i = 0; i < inpArr.length; i++) {
+            int remSum = val - inpArr[i];
+            HashMap<Integer, Integer> diff = new HashMap<>();
+            for (int j = i + 1; j < inpArr.length; j++) {
+                if (diff.containsKey(remSum - inpArr[j]))
+                    return true;
+                diff.put(inpArr[j], j);
+            }
+        }
+        return false;
+    }
+
+    private static void addSortedListToResults(Set<List<Integer>> results, int e, int e1, int e2) {
+        LinkedList<Integer> elems = new LinkedList<>();
+        elems.add(e);
+        elems.add(e1);
+        elems.add(e2);
+        Collections.sort(elems);
+        results.add(elems);
+    }
+
+    public static List<List<Integer>> threeSum(int[] inpArr, int val) {
+        Set<List<Integer>> results = new HashSet<>();
+        if (inpArr == null || inpArr.length < 3)
+            return new ArrayList<>();
+
+        for (int i = 0; i < inpArr.length; i++) {
+            int remSum = val - inpArr[i];
+            HashMap<Integer, Integer> diff = new HashMap<Integer, Integer>();
+            for (int j = i + 1; j < inpArr.length; j++) {
+                int reqSum = remSum - inpArr[j];
+                if (diff.containsKey(reqSum)) {
+                    addSortedListToResults(results, inpArr[i], inpArr[diff.get(reqSum)], inpArr[j]);
+                    diff.remove(reqSum);
+                }
+
+                diff.put(inpArr[j], j);
+            }
+        }
+
+        return new ArrayList<>(results);
+    }
+
+
+    public static void testThreeSumToZero() {
+        int sum = 0;
+        int[] inpArr = {-1, 0, 1, 2, -1, -4};
+        //System.out.println("Three elements with sum = 0 exists? :"+ isThreeSum(inpArr, sum));
+
+        List<List<Integer>> result = threeSum(inpArr, sum);
+        for (List<Integer> l : result) {
+            System.out.print("{ ");
+            for (int i : l) {
+                System.out.print(i);
+                System.out.print(", ");
+            }
+            System.out.println("}");
+        }
+    }
+
+    public static void main(String[] args) {
+        testThreeSumToZero();
+    }
+}
